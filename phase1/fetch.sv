08a17264169156f549a59305cdf77ba3fccc4e31
Eric Song <33282538+EricSongXinLe@users.noreply.github.com>
2025-11-01 16:29:13 -0700

    F/D/icache init

---
+++ phase1/fetch.sv
@@ -0,0 +1,30 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 11/01/2025 04:02:03 PM
+// Design Name: 
+// Module Name: fetch
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module fetch(
+    input clk,
+    input rst
+    );
+    
+reg pc[31:0];
+
+endmodule
---
+++ phase1/icache.sv
@@ -0,0 +1,27 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 11/01/2025 04:02:03 PM
+// Design Name: 
+// Module Name: icache
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module icache(
+    input clk,
+    input rst
+    );
+endmodule
